d589c92cd6abea760e0f10dc2672e70d52fd04f2
Hlau <hugo.lau@live.ca>
2024-10-01 13:35:29 -0400

    Cleaning up assets.py and spotify.py

--- etl_project/pipelines/spotify.py
+++ etl_project/pipelines/spotify.py
@@ -19,75 +19,30 @@ if __name__=='__main__':
     client_id=os.environ.get('CLIENT_ID')
     client_secret=os.environ.get("CLIENT_SECRET")
 
-
-    spotify_api_client = SpotifyApiClient(client_id=client_id, client_secret=client_secret)
-    
-    df_categories = extract_categories(spotify_api_client)
-    print("df_catagories")
-    print(df_categories)
-    print("get list of catagories used to tag items in Spotify on")
-    #print(df_categories)
     #get spotify api
-    spotify_api_client = SpotifyApiClient(client_id=client_id, client_secret=client_secret)   
-    
+    spotify_api_client = SpotifyApiClient(client_id=client_id, client_secret=client_secret)
+
+
+    print("********** Getting New Releases **********")
     df_new_releases = extract_new_releases(spotify_api_client)
-    print("get_new_releases")
-    
-
+ 
     #2 transformation techniques select certain attributes such as album name, album id, release_date, total_tracks, and etc
     #filter and rename select columns from dataframe
     select_album_info = transform_album_info(df_new_releases)
+    df_select_album_info = pd.json_normalize(select_album_info)
 
-    # album_info_list = []
-    # for index, album_info in df_new_releases.iterrows():
-    #     album_info_dict = {
-    #         'album_name': album_info['name'],
-    #         'album_id': album_info['id'],
-    #         'release_date': album_info['release_date'],
-    #         'total_tracks': album_info['total_tracks'],
-    #         'artist_name': album_info['artists'][0]['name'],
-    #         'artist_id': album_info['artists'][0]['id']
-    #     }
-    #     album_info_list.append(album_info_dict)
-    #2 transformation techniques select certain attributes such as album name, album id, release_date, total_tracks, and etc
-    #filter and rename select columns from dataframe
-    select_album_info = transform_album_info(df_new_releases)
-    
     print("********** Here are the top 3 Releases **********")
-    #print(select_album_info)
-    df_select_album_info = pd.json_normalize(select_album_info)
-    print(df_select_album_info)
-    print("********** Here are the top 3 Releases **********")
-    #print(select_album_info)
-    df_select_album_info = pd.json_normalize(select_album_info)
     print(df_select_album_info)
 
-    #get track id
-    print("get album id")
-    # # album_id = []
-    # # for album_info in df_df_album_info_list:
-    # #     album_id.append(album_info["album_id"])
-
-    # album_id = []
-    # for album_info in select_album_info:
-    #     album_id.append(album_info["album_id"])
-    
-    # print(album_id)
-
-    # print("get catalog info from album track")
-    # track_data = []
-    # for album_id in album_id:
-    #     track_data.extend(extract_album_tracks(spotify_api_client, album_id = album_id))
-
+    print("********** Get information about the album track **********")
     track_data = []
     for album_info in select_album_info:
         track_data.extend(extract_album_tracks(spotify_api_client, album_id = album_info["album_id"]))
     
-    #print(track_data)
 
     features = []
     track_popularity = []
-    print("get track details including audio features and popularity for each album")
+    print("get details of track and its audio features")
     for track in track_data:
         #Fetch Audio features
         get_features = extract_audio_features(spotify_api_client, track['id'])
@@ -95,45 +50,22 @@ if __name__=='__main__':
         #Fetch track details to get popularity
         get_popularity = extract_track(spotify_api_client, track['id'])
         track_popularity.append(get_popularity)
-        #track_info = track_data.copy() 
-        # track_info = {
-        #     'album': track_popularity['album']['name'],
-        #     'artist': track_popularity['artists'][0]['name'],
-        #     'track_name': track['name'],
-        #     'track_id': track['id'],          
-        #     'acousticness': features['acousticness'],
-        #     'danceability': features['danceability'],
-        #     'energy': features['energy'],
-        #     'instrumentalness': features['instrumentalness'],
-        #     'liveness': features['liveness'],
-        #     'loudness': features['loudness'],
-        #     'speechiness': features['speechiness'],
-        #     'tempo': features['tempo'],
-        #     'valence': features['valence'],
-        #     'popularity': track_popularity['popularity']  # Fetch popularity
-        # }
-        # data.append(track_info)
+
     df_features = pd.json_normalize(features)
+    df_track_popularity = pd.json_normalize(track_popularity)
+
+    # ********* This is too see the dumps for these files if needed ***********
     # csv_file_path = 'features.csv'
     # df_features.to_csv(csv_file_path,index=False)
-    # #print("features of songs")
-    # #print(df_features.columns.tolist())
-    # #print("track popularity")
-    df_track_popularity = pd.json_normalize(track_popularity)
     # csv_file_path = 'track_popularity.csv'
     # df_track_popularity.to_csv(csv_file_path,index=False)
-    #print(df_track_popularity.columns.tolist())
-    #print(track_popularity)
-    #print(df_track_popularity)
 
-    # merge the two tables (one for audio features, one for track details including popularity)
+    print("finish colleting track info and now combining the files")
+    # Transform - merge the two tables (one for audio features, one for track details including popularity)
     df_merged = pd.merge(left=df_features, right = df_track_popularity, on = ["id"])
-
-    print("finish colleting track info")
-#   print(df_merged.columns.tolist())
     # This transformation technique extracts the artist's name from the first element in the list of dictionaries. It checks if x is not empty before trying to access it
     df_merged['artist_name'] = df_merged['album.artists'].apply(lambda x: x[0]['name'] if x else None)
-    # this transformation technique renames one of the clumns
+    # this transformation technique renames two of the columns
     df_merged = df_merged.rename(columns={"artist_name": "artist", "album.name": "album", "id": "track id"})
     df_selected = df_merged[
         ["album", 
@@ -155,83 +87,13 @@ if __name__=='__main__':
     print("******** Here are the audio characteristics of popular albums and tracks **************")
     print(df_selected.head(50))
 
+    # This is a dump of final file
     csv_file_path = 'selected_data.csv'
     df_selected.to_csv(csv_file_path,index=False)
 
-    #have not used this function yet
+     #have not used this function yet
     df_categories = extract_categories(spotify_api_client)
-    print("get list of catagories used to tag items in Spotify on")
-    #print(df_categories)
-
-    ##### Can get Top 10 tracks for Each Artist ####
-
-    # album_id = []
-    # for album_info in select_album_info:
-    #     album_id.append(album_info["album_id"])
-    
-    # print(album_id)
-
-    # print("get catalog info from album track")
-    # track_data = []
-    # for album_id in album_id:
-    #     track_data.extend(extract_album_tracks(spotify_api_client, album_id = album_id))
-
-    track_data = []
-    for album_info in select_album_info:
-        track_data.extend(extract_album_tracks(spotify_api_client, album_id = album_info["album_id"]))
-    
-    features = []
-    track_popularity = []
-    print("get track details including audio features and popularity for each album")
-    for track in track_data:
-        #Fetch Audio features
-        get_features = extract_audio_features(spotify_api_client, track['id'])
-        features.append(get_features)
-        #Fetch track details to get popularity
-        get_popularity = extract_track(spotify_api_client, track['id'])
-        track_popularity.append(get_popularity)
-    df_features = pd.json_normalize(features)
-    # csv_file_path = 'features.csv'
-    # df_features.to_csv(csv_file_path,index=False)
-    df_track_popularity = pd.json_normalize(track_popularity)
-    # csv_file_path = 'track_popularity.csv'
-    # df_track_popularity.to_csv(csv_file_path,index=False)
-
-    # merge the two tables (one for audio features, one for track details including popularity)
-    df_merged = pd.merge(left=df_features, right = df_track_popularity, on = ["id"])
-
-    print("finish colleting track info")
-#   print(df_merged.columns.tolist())
-    # This transformation technique extracts the artist's name from the first element in the list of dictionaries. It checks if x is not empty before trying to access it
-    df_merged['artist_name'] = df_merged['album.artists'].apply(lambda x: x[0]['name'] if x else None)
-    # this transformation technique renames one of the clumns
-    df_merged = df_merged.rename(columns={"artist_name": "artist", "album.name": "album", "id": "track id"})
-    df_selected = df_merged[
-        ["album", 
-        "artist",
-        "name",
-        "track id",          
-        "acousticness",
-        "danceability",
-        "energy",
-        "instrumentalness",
-        "liveness",
-        "loudness",
-        "speechiness",
-        "tempo",
-        "valence",
-        "popularity"]
-    ]
-
-    print("******** Here are the audio characteristics of popular albums and tracks **************")
-    print(df_selected.head(50))
-
-    csv_file_path = 'selected_data.csv'
-    df_selected.to_csv(csv_file_path,index=False)
-
-    #have not used this function yet
-    df_categories = extract_categories(spotify_api_client)
-    print("get list of catagories used to tag items in Spotify on")
+    #print("get list of catagories used to tag items in Spotify on")
     #print(df_categories)
 
     ##### Can get Top 10 tracks for Each Artist ####
@@ -242,9 +104,6 @@ if __name__=='__main__':
     df_songs = pd.json_normalize(songs)
     #print("songs by artists")
     #print(df_songs)
-   
-       print("songs")
-
     #print("Top 10 tracks for Imagine Dragons")
     #for idx, song in enumerate(songs):
         #print(f"{idx + 1}. {song['name']}")
